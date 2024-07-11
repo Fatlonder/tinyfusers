c098097a342cf2af6c0a5f40b65c6701a9f0fce2
Fatlonder <fatlondercakolli@gmail.com>
2024-07-11 14:02:05 +0200

    test linear

--- tests/linear.py
+++ tests/linear.py
@@ -1,46 +1,14 @@
-import cudnn
-import pytest
+from time import monotonic
 import torch
 import cupy as cp
 import numpy as np
-import itertools
-import torch
 from tinygrad import Tensor
-from time import monotonic
-from tinyfusers.ff.linear import linear
-from looseversion import LooseVersion
-from tinyfusers.ff.linear import linear
-from tinyfusers.ff.linear import linear_cublas
+from tinyfusers.ff.linear import linear, linear_cublas
 from tinyfusers.storage.tensor import Tensor
-
 
 B, M, N, K, i_dtype = 1, 10000, 20000, 1000, torch.float16
 embedding_dim_options = [768, 1024, 1280, 1600]
-input_type_options = [torch.bfloat16, torch.float16]
-
-all_options = [elem for elem in itertools.product(*[embedding_dim_options, input_type_options])]
-
-@pytest.fixture(params=all_options)
-def param_extract(request):
-  return request.param
-
-@pytest.mark.skipif(LooseVersion(cudnn.backend_version_string()) < "8.9.5", reason="LN not supported below cudnn 8.9.5")
-def test_linear(param_extract):
-    embedding_dim, input_type = param_extract
-    if input_type == torch.bfloat16:
-        atol, rtol = 0.125, 0.125
-    else:
-        atol, rtol = 1e-2, 1e-2
-    batch_size = 16
-    (b, m, n, k), input_type = ((1, batch_size, embedding_dim, embedding_dim*4), input_type)
-    X_gpu = torch.randn(b, m, n, requires_grad=False, device="cuda", dtype=input_type)
-    W_gpu = torch.randn(1, n, k, requires_grad=False, device="cuda", dtype=input_type)
-    B_gpu = torch.randn(1, 1, k, requires_grad=False, device="cuda", dtype=input_type)
-
-    Y_expected = torch.nn.functional.linear(X_gpu, W_gpu.squeeze().T, bias=B_gpu.squeeze())
-    Y_actual = linear(X_gpu, W_gpu, B_gpu)
-
-    torch.testing.assert_close(Y_expected[0], torch.from_numpy(cp.asnumpy(Y_actual)).to("cuda"), atol=atol, rtol=rtol)
+input_type_options = [torch.bfloat16, torch.float16, torch.float32]
 
 def test_speedup(B, M, N, K, i_dtype):
     mat1 = torch.randn(B, M, K, requires_grad=False, device="cuda", dtype=i_dtype)
@@ -79,19 +47,23 @@ def test_speedup(B, M, N, K, i_dtype):
     tfmat3 = linear(mat1, mat2, bias1)
     print(f"CUDNN: {monotonic()-start_time}")
 
+    start_time = monotonic()
+    tfmat4 = linear_cublas(Tensor.from_np(mat1).eval(), Tensor.from_np(mat2).eval(), Tensor.from_np(bias1).eval())
+    print(f"cublas: {monotonic()-start_time}")
+
     torch.testing.assert_close(mat3, torch.from_numpy(tmat3).to("cuda"), atol=1e-2, rtol=1e-2)
     torch.testing.assert_close(mat3, torch.from_numpy(cp.asnumpy(cmat3)).to("cuda"), atol=1e-2, rtol=1e-2)
     torch.testing.assert_close(mat3, fmat3, atol=1e-2, rtol=1e-2)
     torch.testing.assert_close(mat3, torch.from_numpy(cp.asnumpy(tfmat3)).to("cuda"), atol=1e-2, rtol=1e-2)
-
+    torch.testing.assert_close(mat3.squeeze(), torch.from_numpy(tfmat4.data.reshape(M, N).to("cuda")), atol=1e-2, rtol=1e-2)
 
 def test_cublas():
-    x = Tensor.from_np(np.ones((2,2)).astype(np.float32)).eval()
-    weight = Tensor.from_np(np.ones((2,2)).astype(np.float32)).eval()
+    M, K, N = 2, 2, 2
+    x = Tensor.from_np(np.ones((K, N)).astype(np.float32)).eval()
+    weight = Tensor.from_np(np.ones((M,K)).astype(np.float32)).eval()
     res = linear_cublas(x, weight)
     print(res.to("cpu").data.reshape(2,2))
 
 if __name__ == "__main__":
-    test_linear((768, torch.float16))
     test_speedup(B, M, N, K, i_dtype)
     test_cublas()
